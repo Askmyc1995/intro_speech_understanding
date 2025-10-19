6eb1396c31feba1c1165ab225fdb22f74ea3ba26
Sojohei <sojohei@SojoheideMacBook-Air.local>
2025-10-19 19:40:58 +0900

    hw1

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -4,6 +4,19 @@ that method, type `help homework2.arithmetic`.
 '''
 
 def arithmetic(x, y):
+    if isinstance(x, str) and isinstance(y, str):
+        return x + y
+    elif isinstance(x, str) and isinstance(y, (float, int)):
+        return x * int(y)
+    elif isinstance(x, (float, int)) and isinstance(y, str):
+        return str(x) + y
+    elif isinstance(x, (float, int)) and isinstance(y, (float, int)):
+        return x * y
+    else:
+        raise TypeError("Unsupported type combination")
+
+    
+    
     """
     Modify this code so that it performs one of four possible functions, 
     as specified in the following table:
@@ -12,5 +25,5 @@ def arithmetic(x, y):
     isinstance(y,str)   return x+y         return str(x)+y
     isinstance(y,float) return x*int(y)    return x*y
     """
-    return 0
+
 
